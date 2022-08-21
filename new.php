ae808af9354375cdbf08ac1fee947cba56cb4116
KristinaBri <59978364+KristinaBri@users.noreply.github.com>
2022-08-21 19:37:42 +0300

    Fix

--- new.php
+++ new.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KMI skaičiuoklė</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<?php ?>
 </head>
 <body>
-    <?php ?>
+    
     <div class="container">
         <div class="row">
             <div class="col-md-6 mx-auto">
